8c714aa0e51c2e3658613f433e49ab1d0c57ccb8
root <root@ansible-deb.c.web1-215116.internal>
2019-02-22 13:15:28 +0000

    Ansible LEMP for Debian 9

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-apt update && apt install git ansible mc -y
+apt update && apt install git mc -y
 mkdir /etc/ansible/roles
 git clone https://github.com/Nurlan199206/ansible-lemp-debian-9 /etc/ansible/roles/ansible-lemp-debian-9
+###########updating Ansible to latest version##################
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/source.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+sudo apt-get update
+sudo apt-get install ansible
